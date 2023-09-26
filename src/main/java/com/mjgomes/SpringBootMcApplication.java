931f5a8434dba41bed87db95edbcb48a1d7497bf
MarcioGomes78 <mjgomes1978@gmail.com>
2023-09-26 20:15:38 -0300

    Criacao do projeto e das classes Produto e Categoria

---
+++ src/main/java/com/mjgomes/SpringBootMcApplication.java
@@ -0,0 +1,13 @@
+package com.mjgomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMcApplication.class, args);
+	}
+
+}
